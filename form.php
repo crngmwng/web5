9c64abb4ccbc6ad25e9e40c724628ba42428935f
Александра Щупоченко <52908598+crngmwng@users.noreply.github.com>
2022-04-19 13:51:25 +0300

    Update form.php

--- form.php
+++ form.php
@@ -22,10 +22,72 @@ if (!empty($messages)) {
 // Далее выводим форму отмечая элементы с ошибками классом error
 // и задавая начальные значения элементов ранее сохраненными.
 ?>
-
-    <form action="" method="POST">
-      <input name="fio" <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print $values['fio']; ?>" />
-      <input type="submit" value="ok" />
+      
+<form action="" method="POST">
+        <label>
+            Имя:<br />
+            <input name="name" <?php if ($errors['name_empty']|| $errors['name_wrong']) {print 'class="error"';} ?> value="<?php print $values['name']; ?>" /> />
+        </label><br />
+        <label>
+            email:<br />
+            <input name="email" <?php if ($errors['email']||$errors['email_empty']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>" /> />
+        </label><br />
+        <select id="year" name="year"></select> <br />
+        <script>for (let year = 1920; year <= 2022; year++) {
+            let options = document.createElement("OPTION");
+            document.getElementById("year").appendChild(options).innerHTML = year;
+          }
+        </script>
+        Пол:
+        <label>
+            <input type="radio" checked="checked"
+                   name="radio-group-1" value="m" />
+            Муж
+        </label>
+        <label>
+            <input type="radio"
+                   name="radio-group-1" value="f" />
+            Жен
+        </label><br />
+        Количество конечностей: <br />
+        <label>
+            <input type="radio" checked="checked"
+                   name="radio-group-2" value="1" />
+            1
+        </label>
+        <label>
+            <input type="radio"
+                   name="radio-group-2" value="2" />
+            2
+        </label>
+        <label>
+            <input type="radio" checked="checked"
+                   name="radio-group-2" value="3" />
+            3
+        </label>
+        <label>
+            <input type="radio"
+                   name="radio-group-2" value="4" />
+            4
+        </label><br />
+        <label>
+            Сверхспособности:
+            <br />
+            <select name="power"
+                    multiple="multiple">
+                <option value="immortality">Бессмертие</option>
+                <option value="pass_thr_walls" selected="selected">Прохождение сквозь стены</option>
+                <option value="levitation" selected="selected">Левитация</option>
+            </select>
+        </label><br />
+        <label>
+           Биография:<br />
+            <textarea name="bio" <?php if ($errors['bio_empty']) {print 'class="error"';} ?> value="<?php print $values['bio']; ?>"></textarea>
+        </label><br />
+        <label><input type="checkbox" checked="checked" name="check-1" />
+            С контрактом ознакомлен
+        </label><br />
+         <input type="submit" value="ok" />
     </form>
-  </body>
+</body>
 </html>
