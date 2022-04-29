bd5fe36ac01f158309798bd48cab1cb37c0ff3e6
Александра Щупоченко <52908598+crngmwng@users.noreply.github.com>
2022-04-29 21:30:40 +0300

    Update form.php

--- form.php
+++ form.php
@@ -41,33 +41,37 @@ if (!empty($messages)) {
         Пол:
         <label>
             <input type="radio" checked="checked"
-                   name="radio-group-1" value="m" />
+                   name="radio-group-1" value="m" <?php if($values['radio-group-1']=="m") {print 'checked';} ?> />
             Муж
         </label>
         <label>
             <input type="radio"
-                   name="radio-group-1" value="f" />
+                   name="radio-group-1" value="f"  <?php if($values['radio-group-1']=="f") {print 'checked';} ?>/>
             Жен
         </label><br />
         Количество конечностей: <br />
         <label>
             <input type="radio" checked="checked"
-                   name="radio-group-2" value="1" />
+                   name="radio-group-2" value="1" 
+                   <?php if($values['radio-group-2']=="1") {print 'checked';} ?>/>
             1
         </label>
         <label>
             <input type="radio"
-                   name="radio-group-2" value="2" />
+                   name="radio-group-2" value="2"
+                   <?php if($values['radio-group-2']=="2") {print 'checked';} ?>/>
             2
         </label>
         <label>
             <input type="radio" checked="checked"
-                   name="radio-group-2" value="3" />
+                   name="radio-group-2" value="3"  
+                   <?php if($values['radio-group-2']=="3") {print 'checked';} ?>/>
             3
         </label>
         <label>
             <input type="radio"
-                   name="radio-group-2" value="4" />
+                   name="radio-group-2" value="4"
+                   <?php if($values['radio-group-2']=="4") {print 'checked';} ?>/>
             4
         </label><br />
         <label>
@@ -82,12 +86,12 @@ if (!empty($messages)) {
         </label><br />
         <label>
            Биография:<br />
-            <textarea name="bio" <?php if ($errors['bio_empty']) {print 'class="error"';} ?> value="<?php print $values['bio']; ?>"></textarea>
+            <textarea name="bio"> <?php if ($errors['bio_empty']) {print 'class="error"';} ?> value="<?php print $values['bio']; ?>"</textarea>
         </label><br />
-        <label><input type="checkbox" checked="checked" name="check-1" />
+        <label><input type="checkbox" checked="checked" name="check-1" <?php if(!empty($values['check-1'])){print 'checked';} ?> />
             С контрактом ознакомлен
         </label><br />
-         <input type="submit" value="ok" />
+         <input type="submit" value="ok"  />
     </form>
 </body>
 </html>
